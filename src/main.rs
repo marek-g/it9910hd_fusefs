6ffa501e3eb3c1fdb89442bf1cf517a1b846127c
Marek G <marek@nomail.com>
2019-08-16 18:48:58 +0200

    Add fuse crate.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,159 @@
+use fuse::ReplyEmpty;
+use fuse::ReplyOpen;
+use fuse::{
+    FileAttr, FileType, Filesystem, ReplyAttr, ReplyData, ReplyDirectory, ReplyEntry, Request,
+};
+use libc::ENOENT;
+use std::env;
+use std::ffi::OsStr;
+use std::time::{Duration, UNIX_EPOCH};
+
 mod it9910hd_driver;
 use it9910hd_driver::*;
 
+const TTL: Duration = Duration::from_secs(1); // 1 second
+
+const DIR_ATTR: FileAttr = FileAttr {
+    ino: 1,
+    size: 0,
+    blocks: 0,
+    atime: UNIX_EPOCH, // 1970-01-01 00:00:00
+    mtime: UNIX_EPOCH,
+    ctime: UNIX_EPOCH,
+    crtime: UNIX_EPOCH,
+    kind: FileType::Directory,
+    perm: 0o755,
+    nlink: 2,
+    uid: 501,
+    gid: 20,
+    rdev: 0,
+    flags: 0,
+};
+
+const HDMI_STREAM_TS_ATTR: FileAttr = FileAttr {
+    ino: 2,
+    size: 512 * 1024 * 1024 * 1024,
+    blocks: 0,
+    atime: UNIX_EPOCH, // 1970-01-01 00:00:00
+    mtime: UNIX_EPOCH,
+    ctime: UNIX_EPOCH,
+    crtime: UNIX_EPOCH,
+    kind: FileType::RegularFile,
+    perm: 0o644,
+    nlink: 1,
+    uid: 501,
+    gid: 20,
+    rdev: 0,
+    flags: 0,
+};
+
+struct IT9910HD_FS {
+    next_fh: u64,
+}
+
+impl IT9910HD_FS {
+    pub fn new() -> Self {
+        IT9910HD_FS {
+            next_fh: 0,
+        }
+    }
+}
+
+impl Filesystem for IT9910HD_FS {
+    fn lookup(&mut self, _req: &Request, parent: u64, name: &OsStr, reply: ReplyEntry) {
+        if parent == 1 && name.to_str() == Some("hdmi_stream.ts") {
+            reply.entry(&TTL, &HDMI_STREAM_TS_ATTR, 0);
+        } else {
+            reply.error(ENOENT);
+        }
+    }
+
+    fn getattr(&mut self, _req: &Request, ino: u64, reply: ReplyAttr) {
+        match ino {
+            1 => reply.attr(&TTL, &DIR_ATTR),
+            2 => reply.attr(&TTL, &HDMI_STREAM_TS_ATTR),
+            _ => reply.error(ENOENT),
+        }
+    }
+
+    fn readdir(
+        &mut self,
+        _req: &Request,
+        ino: u64,
+        _fh: u64,
+        offset: i64,
+        mut reply: ReplyDirectory,
+    ) {
+        if ino != 1 {
+            reply.error(ENOENT);
+            return;
+        }
+
+        let entries = vec![
+            (1, FileType::Directory, "."),
+            (1, FileType::Directory, ".."),
+            (2, FileType::RegularFile, "hdmi_stream.ts"),
+        ];
+
+        for (i, entry) in entries.into_iter().enumerate().skip(offset as usize) {
+            // i + 1 means the index of the next entry
+            reply.add(entry.0, (i + 1) as i64, entry.1, entry.2);
+        }
+        reply.ok();
+    }
+
+    fn open(&mut self, _req: &Request, _ino: u64, _flags: u32, reply: ReplyOpen) {
+        println!("Open");
+        reply.opened(0, 0);
+    }
+
+    fn read(
+        &mut self,
+        _req: &Request,
+        ino: u64,
+        _fh: u64,
+        offset: i64,
+        _size: u32,
+        reply: ReplyData,
+    ) {
+        if ino == 2 {
+            println!("Read: {}, {}, {}", _fh, offset, _size);
+            let vec = vec![0u8; _size as usize];
+            reply.data(&vec);
+        } else {
+            reply.error(ENOENT);
+        }
+    }
+
+    fn release(
+        &mut self,
+        _req: &Request,
+        _ino: u64,
+        _fh: u64,
+        _flags: u32,
+        _lock_owner: u64,
+        _flush: bool,
+        reply: ReplyEmpty,
+    ) {
+        println!("Close: {}", _fh);
+        reply.ok();
+    }
+}
+
 fn main() {
-    match run() {
+    //env_logger::init();
+    let mountpoint = env::args_os().nth(1).unwrap();
+    let options = ["-o", "ro", "-o", "fsname=it9910hd_fs"]
+        .iter()
+        .map(|o| o.as_ref())
+        .collect::<Vec<&OsStr>>();
+
+    let it9910hd_fs = IT9910HD_FS::new();
+
+    fuse::mount(it9910hd_fs, mountpoint, &options).unwrap();
+
+    /*match run() {
         Err(err) => panic!("Cannot open Encoder: {}", err),
         _ => (),
-    };
+    };*/
 }
